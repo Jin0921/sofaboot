f49194942ed7465e51d63f5687943cd877c1c8c7
Jin0921 <18749855536@163.com>
2021-09-08 15:35:15 +0800

    第一次提交

---
+++ src/main/java/com/example/sofaboot/SofaBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.sofaboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SofaBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SofaBootApplication.class, args);
+    }
+
+}
